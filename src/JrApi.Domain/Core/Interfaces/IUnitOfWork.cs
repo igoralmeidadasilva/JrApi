20913be196f1925c9f0c72f1184598d688c85d92
Igor <Igor.almeidadasilva13@gmail.com>
2024-09-15 20:27:33 -0300

    Implementation :IUnitOfWork

---
+++ src/JrApi.Domain/Core/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,9 @@
+namespace JrApi.Domain.Core.Interfaces;
+
+public interface IUnitOfWork : IDisposable
+{
+    void BeginTransaction();
+    void Commit();
+    void Rollback();
+    void SaveChanges();
+}
