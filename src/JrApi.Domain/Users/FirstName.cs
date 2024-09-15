2c58a037bee4e13314cb69bf4e4aee348dd7c572
Igor <Igor.almeidadasilva13@gmail.com>
2024-09-15 20:28:32 -0300

    Implementation: FirstName ValueObject for User Entity

---
+++ src/JrApi.Domain/Users/FirstName.cs
@@ -0,0 +1,29 @@
+﻿using JrApi.Domain.Core;
+using JrApi.Domain.Core.Abstractions;
+using static JrApi.Domain.Constants.Constraints.User;
+
+namespace JrApi.Domain.Users;
+
+public sealed record FirstName : ValueObject
+{
+    public string Value { get; init; }
+
+    private FirstName(string value)
+    {
+        Value = value;
+    }
+
+    public static FirstName Create(string value)
+    { 
+        ArgumentValidator.ThrowIfNullOrWhitespace(value, nameof(value));
+        ArgumentValidator.ThrowIfOutOfRange(value.Length, nameof(value), 0, FIRST_NAME_MAX_SIZE);
+        return new(value);
+    }
+
+    public static implicit operator string(FirstName firstName) => firstName?.Value ?? string.Empty;
+
+    protected override IEnumerable<object> GetEqualityComponents()
+    {
+        yield return Value;
+    }
+}
