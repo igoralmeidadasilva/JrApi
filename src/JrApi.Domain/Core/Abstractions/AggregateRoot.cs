45566c55560e6e5fdd3cfd3dac5fae14c2d414a3
Igor <Igor.almeidadasilva13@gmail.com>
2024-09-15 20:25:47 -0300

    Implementation: AggregateRoot<T> generic absrtact class. This class is still without constructors

---
+++ src/JrApi.Domain/Core/Abstractions/AggregateRoot.cs
@@ -0,0 +1,13 @@
+using JrApi.Domain.Core.Interfaces;
+
+namespace JrApi.Domain.Core.Abstractions;
+
+public abstract class AggregateRoot<T> : Entity<T>
+{
+    private readonly List<IDomainEvent> _events = [];
+    public IReadOnlyCollection<IDomainEvent> Events => _events.AsReadOnly();
+
+    public void ClearDomainEvents() => _events.Clear();
+
+    public void AddDomainEvent(IDomainEvent domainEvent) => _events.Add(domainEvent);
+}
