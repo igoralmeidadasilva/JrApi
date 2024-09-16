373eebfdc455194f356f8dbc33f687bb9998d22d
Igor <Igor.almeidadasilva13@gmail.com>
2024-09-16 00:08:06 -0300

    Implementation: Erro static Class for Result Pattern

---
+++ src/JrApi.Domain/Core/Abstractions/Results/Error.cs
@@ -0,0 +1,17 @@
+namespace JrApi.Domain.Core.Abstractions.Results;
+
+public record Error
+{
+    public string Code { get; set; }
+    public string Message { get; set; }
+    public ErrorType Type { get; set; }
+
+    public Error(string code, string message, ErrorType type = ErrorType.Failure)
+    {
+        Code = code;
+        Message = message;
+        Type = type;
+    }
+
+    public static readonly Error None = new(string.Empty, string.Empty, ErrorType.None);
+}
