12336707332564f3fb275055e279b226626c74f0
Igor <Igor.almeidadasilva13@gmail.com>
2024-09-15 21:08:04 -0300

    Update: AggregateRoot abstract class add contructors Methods

--- src/JrApi.Domain/Core/Abstractions/AggregateRoot.cs
+++ src/JrApi.Domain/Core/Abstractions/AggregateRoot.cs
@@ -7,6 +7,12 @@ public abstract class AggregateRoot<T> : Entity<T>
     private readonly List<IDomainEvent> _events = [];
     public IReadOnlyCollection<IDomainEvent> Events => _events.AsReadOnly();
 
+    public AggregateRoot(Guid id) : base(id)
+    { }
+
+    public AggregateRoot() : base()
+    { }
+
     public void ClearDomainEvents() => _events.Clear();
 
     public void AddDomainEvent(IDomainEvent domainEvent) => _events.Add(domainEvent);
