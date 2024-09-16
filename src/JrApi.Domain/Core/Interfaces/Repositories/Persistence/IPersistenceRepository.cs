821e2b358f06ad7d80eea17e9b19ac34041e50f6
Igor <Igor.almeidadasilva13@gmail.com>
2024-09-16 00:22:39 -0300

    Implementation: IPersistence Interface

---
+++ src/JrApi.Domain/Core/Interfaces/Repositories/Persistence/IPersistenceRepository.cs
@@ -0,0 +1,11 @@
+using JrApi.Domain.Core.Abstractions;
+
+namespace JrApi.Domain.Core.Interfaces.Repositories.Persistence;
+
+public interface IPersistenceRepository<TEntity> where TEntity : Entity<TEntity>
+{
+    Task<Guid> AddAsync(TEntity entity, CancellationToken cancellationToken = default);
+    Task AddRangeAsync(IEnumerable<TEntity> entities, CancellationToken cancellationToken = default);
+    Task UpdateAsync(TEntity entity, CancellationToken cancellationToken = default);
+    Task DeleteAsync(Guid id, CancellationToken cancellationToken = default);
+}
