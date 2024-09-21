5019d4ef4ec69f9099ae3b23a534accc60fdde59
Igor <Igor.almeidadasilva13@gmail.com>
2024-09-21 15:52:52 -0300

    Implementation: UserReadOnlyRepository

---
+++ src/JrApi.Infrastructure/Repositories/ReadOnly/UserReadOnlyRepository.cs
@@ -0,0 +1,17 @@
+﻿using JrApi.Domain.Core.Interfaces.Repositories.ReadOnly;
+using JrApi.Domain.Entities.Users;
+using JrApi.Infrastructure.Context;
+
+namespace JrApi.Infrastructure.Repositories.ReadOnly;
+
+public sealed class UserReadOnlyRepository : BaseReadOnlyRepository<User>, IUserReadOnlyRepository
+{
+    public UserReadOnlyRepository(ApplicationContext context) : base(context)
+    {
+    }
+
+    public Task<bool> EmailExistsAsync(string email, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+}
